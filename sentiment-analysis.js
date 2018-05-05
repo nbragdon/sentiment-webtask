42d557a58f44d82c7f61fa435a49da56f1312545
Nick Bragdon <nrbragdo@ncsu.edu>
2018-05-05 16:36:16 -0700

    Updating sentiment-analysis.js

--- sentiment-analysis.js
+++ sentiment-analysis.js
@@ -10,39 +10,44 @@ const logger = bunyan.createLogger({ name: "sentiment" });
 const POSITIVE_SENTIMENT_EMOJI = 'thumbsup';
 const NEGATIVE_SENTIMENT_EMOJI = 'thumbsdown';
 
+const sentiment = new Sentiment();
+const slackWebClient = new WebClient(context.secrets.slackSentimentApiToken);
+
 module.exports = function (context, cb) {
     logger.info({ message: 'Entered sentiment handler', contextBody: context.body })
-    let sentiment = new Sentiment();
-    let user = context.body.event.user,
-        messageText = context.body.event.text,
-        channel = context.body.event.channel,
-        token = context.body.token;
-
-    logger.info({ user: user, messageText: messageText, channel: channel });
-
-    let messageSentiment = sentiment.analyze(messageText);
-
-    logger.info({ messageSentiment: messageSentiment })
-
-    const slackWebClient = new WebClient(context.secrets.slackSentimentApiToken);
-    const conversationId = channel;
-
-    let reactionEmojiName = null;
-
-    if (messageSentiment.score < 0) {
-        reactionEmojiName = NEGATIVE_SENTIMENT_EMOJI;
-    } else if (messageSentiment.score > 0) {
-        reactionEmojiName = POSITIVE_SENTIMENT_EMOJI;
-    }
-
-    logger.info({ reactionEmojiName: reactionEmojiName })
-
-    if (context.body.event.username != 'bot' && reactionEmojiName !== null) {
-        slackWebClient.reactions.add({ name: reactionEmojiName, channel: conversationId, timestamp: context.body.event.ts })
-            .then((res) => {
-                logger.log({ responseTime: res.ts, message: 'Message Sent' });
-            })
-            .catch(logger.error);
+    try {
+      let user = context.body.event.user,
+          messageText = context.body.event.text,
+          channel = context.body.event.channel,
+          token = context.body.token,
+          reactionEmojiName = null;
+  
+      logger.info({ user: user, messageText: messageText, channel: channel });
+      
+      if (context.secrets.slackAppVerificationToken !== token) {
+        throw Error('Invalid verification token')
+      }
+  
+      let messageSentiment = sentiment.analyze(messageText);
+  
+      logger.info({ messageSentiment: messageSentiment })
+  
+      if (messageSentiment.score < 0) {
+          reactionEmojiName = NEGATIVE_SENTIMENT_EMOJI;
+      } else if (messageSentiment.score > 0) {
+          reactionEmojiName = POSITIVE_SENTIMENT_EMOJI;
+      }
+  
+      logger.info({ reactionEmojiName: reactionEmojiName })
+  
+      if (context.body.event.username != 'bot' && reactionEmojiName !== null) {
+          slackWebClient.reactions.add({ name: reactionEmojiName, channel: channel, timestamp: context.body.event.ts })
+              .then((res) => {
+                  logger.log({ responseTime: res.ts, message: 'Message Sent' });
+              })
+      }
+    } catch (err) {
+      logger.error(err)
     }
 
     cb(null, context.body.challenge);
